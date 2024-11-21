68f9f460085c2181e933a939336f16cdf56ee2a0
GAROSDEV <garzonoscar7@gmail.com>
2024-11-20 21:40:30 -0500

    Mejora la función de actualización de citas con manejo de errores y registro de datos

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -8,12 +8,47 @@ export default function Quotes() {
   useEffect(() => {
     fetch("http://localhost:4000/api/citas")
       .then((response) => response.json())
-      .then((data) => setQuotes(data))
+      .then((data) => setQuotes(data) || console.log("Quotes fetched:", data))
       .catch((error) => console.error("Error fetching quotes:", error));
   }, []);
 
-  const handleUpdate = (id) => {
+  const handleUpdate = async (id) => {
     console.log("Actualizar cita con ID:", id);
+    try {
+      if (!quotes.length) {
+        throw new Error("No hay citas disponibles para actualizar.");
+      }
+
+      const quote = quotes.find((quotes) => quotes.id_cita === id);
+      if (!quote) {
+        throw new Error(`No se encontró ninguna cita con el ID: ${id}`);
+      }
+
+      const cliente = quote.id_cliente.id_cliente;
+      const manicurista = quote.id_manicurista.id_manicurista;
+
+      const fecha = new Date(quote.fecha);
+      const response = await fetch(`http://localhost:4000/api/citas/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fecha: prompt(
+            "Ingrese la nueva fecha de la cita: (AA-MM-DD)",
+            `${fecha.toISOString().split("T")[0]}`
+          ),
+          hora: prompt("Ingrese la nueva hora de la cita:", quote.hora),
+          id_cliente: cliente,
+          id_manicurista: manicurista,
+        }),
+      });
+
+      const data = await response.json();
+      console.log("Cita actualizada:", data);
+    } catch (error) {
+      console.error("Error updating quote:", error);
+    }
   };
 
   const handleDelete = (id) => {
