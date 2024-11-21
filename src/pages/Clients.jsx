8d2b86facdfb85339c8234a6347d23d2ca1f30d8
GAROSDEV <garzonoscar7@gmail.com>
2024-11-21 17:53:00 -0500

    Añade manejo de errores y confirmaciones en la actualización y eliminación de clientes

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -15,12 +15,51 @@ export default function Clients() {
       .catch((error) => console.error("Error fetching clients:", error));
   }, []);
 
-  const handleUpdate = (id) => {
+  const handleUpdate = async (id) => {
     console.log("Actualizar cliente con ID:", id);
+    try {
+      const client = clients.find((client) => client.id_cliente === id);
+      const nombreCompleto = client.nombre_completo;
+      const correo = client.correo;
+      const telefono = client.telefono;
+      const response = await fetch(`http://localhost:4000/api/clientes/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nombre_completo: prompt(
+            "Ingrese el nuevo nombre completo del cliente:",
+            nombreCompleto
+          ),
+          correo: correo,
+          telefono: prompt("Ingrese el nuevo teléfono del cliente:", telefono),
+        }),
+      });
+
+      const data = await response.json();
+      console.log("Cliente actualizado:", data);
+    } catch (error) {
+      console.error("Error updating client:", error);
+    }
   };
 
   const handleDelete = (id) => {
     console.log("Eliminar cliente con ID:", id);
+    try {
+      if (!clients.length) {
+        throw new Error("No hay clientes disponibles para eliminar.");
+      }
+
+      fetch(`http://localhost:4000/api/clientes/${id}`, {
+        method: "DELETE",
+      }).then(() => {
+        const newClients = clients.filter((client) => client.id_cliente !== id);
+        setClients(newClients);
+      });
+    } catch (error) {
+      console.error("Error deleting client:", error);
+    }
   };
 
   return (
