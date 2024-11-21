29ecb6d26071b2a68033818bad033723b424636c
GAROSDEV <garzonoscar7@gmail.com>
2024-11-21 16:09:13 -0500

    Añade componente Footer con logo y estilos personalizados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,17 @@
+import logo from "../assets/garosdev.png";
+import "../styles/Footer.css";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="footer">
+        <h6>
+          Desarrollado con ❤️ <span>&#11044;</span> GarosDev
+        </h6>
+        <div className="logo-container">
+          <img src={logo} alt="GarosDev" className="logo" />
+        </div>
+      </footer>
+    </div>
+  );
 }
